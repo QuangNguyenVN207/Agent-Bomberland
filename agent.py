7189aaade0d6cfad107aa8d6a15bcd26ef8d718e
QuangNguyen <nguyentranquang171107@gmail.com>
2026-05-23 00:34:17 +0700

    Add libs

--- agent.py
+++ agent.py
@@ -1,3 +1,10 @@
+from .random_agent import RandomAgent
+from .simple_rule_agent import SimpleRuleAgent
+from .smarter_rule_agent import SmarterRuleAgent
+from .genius_rule_agent import GeniusRuleAgent
+from .box_farmer_agent import BoxFarmerAgent
+from .tactical_rule_agent import TacticalRuleAgent
+
 class Agent:
     def __init__(self, agent_id: int):
         # agent_id: 0, 1, 2, or 3
@@ -7,5 +14,5 @@ class Agent:
         # obs: dict containing 'map', 'players', 'bombs'
         # Returns: int in [0, 5]
         my_data = obs["players"][self.agent_id]
-        
+
         return 0
